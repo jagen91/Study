a08aa3b68196971402621d42e13d5a95242a2c6a
jagen91 <132800238+jagen91@users.noreply.github.com>
2023-06-08 17:32:46 +0900

    Add files via upload

---
+++ Exam_0608_collection_교재 타이핑/ch15_sec03_exam02/ch15_sec03_exam02/ch15_sec03_exam02/HashSetExample.java
@@ -0,0 +1,19 @@
+package ch15_sec03_exam02;
+
+
+import java.util.*;
+
+public class HashSetExample {
+    public static void main(String[] args)
+    {
+        // HashSet 컬렉션 생성
+        Set<Member> set = new HashSet<Member>();
+
+        // Member 객체 저장
+        set.add(new Member("홍길동", 30));
+        set.add(new Member("홍길동", 30));
+
+        // 저장된 객체 수 출력
+        System.out.println("총 객체 수 : " + set.size());
+    }
+}
---
+++ Exam_0608_collection_교재 타이핑/ch15_sec_02_exam02/ch15_sec02_exam02/VectorExample.java
@@ -0,0 +1,62 @@
+package ch15_sec02_exam02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class VectorExample {
+    public static void main(String[] args) {
+        //Vector 컬렉션 생성
+        List<Board> list = new Vector<>();
+
+        //작업 스레드 객체 생성
+        Thread threadA = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                // 객체 1000개 추가
+                for (int i = 0; i < 1000; i++)
+                {
+                    list.add(new Board("제목"+i, "내용"+i, "글쓴이"+i));
+                }
+            }
+        };
+
+        // 작업 스레드 객체 생성
+        Thread threadB = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                for (int i = 1000; i < 2000; i++)
+                {
+                    list.add(new Board("제목"+i, "내용"+i, "글쓴이"+i));
+                }
+            }
+        };
+
+        // 작업 스레드 실행
+        threadA.start();
+        threadB.start();
+
+        // 작업 스레드들이 모두 종료될 때까지 메인 스레드를 기다리게 함
+
+        try
+        {
+            threadA.join();
+            threadB.join();
+        }
+        catch(Exception e)
+        {
+
+        }
+
+        // 저장된 총 객체 수 얻기
+        int size = list.size();
+        System.out.println("총 객체 수 : " + size);
+        System.out.println();
+
+
+    }
+}
